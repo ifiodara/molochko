8b86d4a5a99e7b94e793ce358fc884e23afa2387
Illia Fiodarau <ilya.fyodorov95@mail.ru>
2018-07-12 03:22:54 -0700

    added pagination to p24.by extractor

--- bin/p24_extractor.py
+++ bin/p24_extractor.py
@@ -4,19 +4,29 @@ import csv
 from datetime import datetime
 
 
-def drug_data():
-    quote_page = 'http://p24.by/goods/filter/?category_vid=2&category_cats=11&category_podcat=79'
-    page = requests.get(quote_page)
 
+def drug_data(quote_page):
+    #quote_page = 'http://p24.by/goods/filter/?category_vid=2&category_cats=11&category_podcat=79'
+    page = requests.get(quote_page)
     soup = BeautifulSoup(page.text,'html.parser')
+    if (soup.find('div',class_='navigation top').find('ul',id='yw0',class_='pagination').find_all('li')[4].has_attr('class')) and (soup.find('div',class_='navigation top').find('ul',id='yw0',class_='pagination').find_all('li',class_='disabled') is not None):
+        changer = 1
+    else:
+        changer = 0
     product_columns = soup.find(class_='row items').find_all('div',class_='card-product-column')
     resArr = []
     for product_column in product_columns:
         res = []
         price = product_column.find('span',class_='price').find('span',class_='value red').text
         res.append(price)
-        price_old = product_column.find('span',class_='price-old').find('span',class_='value').text
-        price_old_currency = product_column.find('span',class_='price-old').find('span',class_='currency').text
+        try:
+            price_old = product_column.find('span',class_='price-old').find('span',class_='value').text
+        except AttributeError:
+            price_old = ''
+        try:
+            price_old_currency = product_column.find('span',class_='price-old').find('span',class_='currency').text
+        except:
+            price_old_currency = ''
         res.append(price_old)
         res.append(price_old_currency)
         name_elem = product_column.find('h2',class_='title-product').find('a')
@@ -27,13 +37,27 @@ def drug_data():
         extract_ts = datetime.now()
         res.append(extract_ts)
         resArr.append(res)
-    return resArr
+    return resArr,changer
 
-result = drug_data()
+#link = http://p24.by/goods/filter/?category_vid=2&category_cats=11&category_podcat=79&CatalogProducts_page=2
+linkbase = 'http://p24.by/goods/filter/?category_vid=2&category_cats=11&category_podcat=79'
+changer = 0
+result = []
+for i in range (1,6):
+    url = linkbase+'&CatalogProducts_page='+str(i)
+    temp_res = []
+    temp_res, changer = drug_data(url)
+    result.append(temp_res)
+    print(changer)
+    if changer==1:
+        break
+    else:
+        continue
 
-with open('data/result_p24.csv','w') as csv_file:
-    writer = csv.writer(csv_file, delimiter='±',quoting=csv.QUOTE_NONE)
-    for line in result:
-        writer.writerow(line)
 
+with open('/home/ilya/dev/projects/molochko/data/result_p24.csv','w') as csv_file:
+    writer = csv.writer(csv_file, delimiter='±',quoting=csv.QUOTE_NONE, escapechar='\\')
+    for lines in result:
+        for line in lines:
+            writer.writerow(line)
 
